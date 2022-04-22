be7e215e34d9d73b03cefb4714428802f24a7117
chevyriet <chevyriet040104@gmail.com>
2022-04-22 17:11:38 +0200

    Added an update functionality, improved the delete functionality and added error message for viewing profile

--- index.js
+++ index.js
@@ -22,29 +22,45 @@ app.get("/", (req, res) => {
   });
 });
 
-//handling a post request for a new user
-app.post("/api/user", (req, res) => {
-  let user = req.body;
-  id++;
-  user = {
-    id,
-    ...user,
-  };
-  console.log(user);
-  database.push(user);
-  res.status(201).json({
-    status: 201,
-    result: database,
+//getting a users profile, if it has a valid JWT token (endpoint not realized yet)
+app.all("/api/user/profile/*", (req, res) => {
+  res.status(401).json({
+    status: 401,
+    result: "Cant fetch user profile as this functionality has not been realized yet",
   });
 });
 
-//handling a get request for a user by userId
+//handling a post request for a new user
+app.post("/api/user", (req, res) => {
+  let user = req.body;
+  let sameEmailUser = database.filter((item) => item.emailAdress == user.emailAdress);
+  console.log(sameEmailUser);
+  if(sameEmailUser < 1){
+    id++;
+    user = {
+      id,
+      ...user,
+    };
+    database.push(user);
+    res.status(201).json({
+      status: 201,
+      result: user,
+    });
+  } else {
+    res.status(401).json({
+      status: 401,
+      result: `Could not add user, a user with the following email already exists: ${user.emailAdress}`,
+    });
+  }
+
+});
+
+//getting a user by id
 app.get("/api/user/:userId", (req, res, next) => {
   const userId = req.params.userId;
   console.log(`User with ID ${userId} requested`);
   let user = database.filter((item) => item.id == userId);
   if (user.length > 0) {
-    console.log(user);
     res.status(200).json({
       status: 200,
       result: user,
@@ -52,37 +68,57 @@ app.get("/api/user/:userId", (req, res, next) => {
   } else {
     res.status(401).json({
       status: 401,
-      result: `User with ID ${userId} not found`,
+      result: `User with ID ${userId} could not be found`,
     });
   }
 });
 
-//deleting a user by userId
+//deleting a user by id
 app.delete("/api/user/:userId", (req, res, next) => {
   const userId = req.params.userId;
   console.log(`User with ID ${userId} requested to be deleted`);
   let result = database.filter((item) => item.id == userId);
   if (result.length > 0) {
     let user = result[0];
-
     const index = database.indexOf(user);
-    console.log(user);
     if (index < 0) return;
-
     database.splice(index, 1);
+    res.status(201).json({
+      status: 201,
+      result: `User with ID ${userId} has been deleted`,
+    });
+  } else {
+    res.status(401).json({
+      status: 401,
+      result: `User with ID ${userId} not found, and could not be deleted`,
+    });
+  }
+})
 
-    console.log(`User with ID ${userId} has been deleted`);
-    res.status(200).json({
-      status: 200,
+//updating a user by id
+app.put("/api/user/:userId", (req, res, next) => {
+  const userId = req.params.userId;
+  const updateUser = req.body;
+  console.log(`User with ID ${userId} requested to be updated`);
+  let oldUser = database.filter((item) => item.id == userId);
+  if (oldUser.length > 0) {
+    const index = database.indexOf(oldUser[0]);
+    user = {
+      id,
+      ...updateUser,
+    };
+    database[index] = user;
+    res.status(201).json({
+      status: 201,
       result: user,
     });
   } else {
     res.status(401).json({
       status: 401,
-      result: `User with ID ${userId} not found, and couldnt be deleted`,
+      result: `User with ID ${userId} not found, and couldnt be updated`,
     });
   }
-})
+});
 
 //getting all users
 app.get("/api/user", (req, res, next) => {
@@ -105,5 +141,5 @@ app.all("*", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Share A Meal API app listening on port ${port}`);
 });
