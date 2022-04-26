7c072f34ca2d36cea352e4305ee566e1187e9144
chevyriet <chevyriet040104@gmail.com>
2022-04-26 12:40:12 +0200

    Added testing

--- index.js
+++ index.js
@@ -35,3 +35,5 @@ app.use((err,req,res,next) => {
 app.listen(port, () => {
   console.log(`Share A Meal API app listening on port ${port}`);
 });
+
+module.exports = app;
---
+++ test/integration/user.controller.test.js
@@ -0,0 +1,34 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../../index");
+let database = [];
+
+chai.should();
+chai.use(chaiHttp);
+
+describe("Manage Users /api/user",() => {
+    describe("UC-201 add users /api/user", ()=> {
+        beforeEach((done) => {
+            database = [];
+            done();
+        });
+
+        it(" TC-201-1 When a required input is missing, a valid error should be returned", (done) => {
+            chai.request(server).post("/api/user").send({
+                //firstName ontbreekt
+                lastName: "Rietveld",
+                street: "Van Wenastraat 31",
+                city: "Giessenburg",
+                password: "secret",
+                emailAdress: "chevy@gmail.com"
+            })
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(400)
+                result.should.be.a("string").that.equals("First name must be a string");
+                done();
+            });
+        });
+    });
+});
