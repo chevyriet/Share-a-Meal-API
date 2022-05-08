893e4d7458f1b92d547568d328ced9b73d419a7f
chevyriet <chevyriet040104@gmail.com>
2022-05-08 13:52:35 +0200

    Added tests for all user usecases except UC-202, also small changes like also having user validation on update phonenumber now

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,7 +13,7 @@ let controller={
             //password contains 8-15 characters which contains at least one lower- and uppercase letter, one special character and one digit
             assert.match(password, /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/, "Password must contain 8-15 characters which contains at least one lower- and uppercase letter, one special character and one digit");
             //emailAdress must be valid (found this regex online, not aware of all details)
-            assert.match(emailAdress, /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "The provided Emailadress must be valid");
+            assert.match(emailAdress, /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "The provided Emailadress format is invalid");
 
             assert(typeof firstName === "string", "First name must be a string");
             assert(typeof lastName === "string", "Last name must be a string");
@@ -21,6 +21,23 @@ let controller={
             assert(typeof emailAdress === "string", "Email adress must be a string");
             assert(typeof street === "string", "Street must be a string");
             assert(typeof city === "string", "City must be a string");
+            next();
+        } catch(err){
+            const error={
+                status: 400,
+                result: err.message
+            };
+            next(error);
+        }
+    },
+    //validates a user before being updated, gets called after the validateUser method and checks on phone number because its required when updating but not when creating a user
+    validateUpdateUser:(req,res,next)=>{
+        let user = req.body;
+        let { phoneNumber } = user;
+        try{
+            assert(typeof phoneNumber === "string", "Phonenumber must be a string");
+            //regex for phonenumber must be 10 digits long
+            assert.match(phoneNumber, /^\d{10}$/, "Phonenumber must be 10 digits long, example: 0612345678")
             next();
         } catch(err){
             const error={
@@ -40,13 +57,13 @@ let controller={
                     connection.release();
                     res.status(401).json({
                         status: 401,
-                        result: `Could not add user, the email ${user.emailAdress} has already been taken`
+                        result: `Could not add user, the email has already been taken`
                     })
                 } else {
                     connection.release();
                     res.status(201).json({
                         status: 201,
-                        result: `User has been succesfully registered.`,
+                        result: `User has been succesfully registered`,
                     })
                 }
             })
@@ -102,20 +119,19 @@ let controller={
         console.log(`User with ID ${userId} requested to be deleted`);
         dbconnection.getConnection(function(err, connection) {
             if (err) throw err;
-            connection.query('SELECT * FROM user WHERE id = ?;', [userId], function (error, results, fields) {
-                if (err) throw err;
-                user = results;
-            });
+
             connection.query('DELETE FROM user WHERE id = ?;', [userId], function (error, results, fields) {
                 connection.release();
+                if (error) throw error;
+
                 if(results.affectedRows > 0){
-                    res.status(201).json({
-                    status: 201,
-                    result: user,
+                    res.status(200).json({
+                    status: 200,
+                    result: `User with ID ${userId} succesfully deleted`,
                     });
                 } else {
-                    res.status(401).json({
-                        status: 401,
+                    res.status(404).json({
+                        status: 404,
                         result: `User with ID ${userId} not found, and could not be deleted`,
                     });
                 }
@@ -139,15 +155,15 @@ let controller={
                 }
                 if(results.affectedRows>0){
                     connection.query('SELECT * FROM user WHERE id = ?;', [userId], function (error, results, fields) {
-                        res.status(201).json({
-                            status: 201,
-                            result: results,
+                        res.status(200).json({
+                            status: 200,
+                            result: results[0],
                         });
                     });
                 } else {
-                    res.status(401).json({
-                        status: 401,
-                        result: `Update failed, user does not exist`
+                    res.status(400).json({
+                        status: 400,
+                        result: `Update failed, user with ID ${userId} does not exist`
                     })
                 }
             });
--- test/integration/user.controller.test.js
+++ test/integration/user.controller.test.js
@@ -1,25 +1,39 @@
+process.env.DB_DATABASE = process.env.DB_DATABASE || 'share-a-meal-testdb'
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const server = require("../../index");
-let database = [];
+require('dotenv').config();
+const dbconnection = require("../../database/dbconnection");
+const { assert } = require("chai");
 
 chai.should();
 chai.use(chaiHttp);
 
 describe("Manage Users /api/user",() => {
-    describe("UC-201 add users /api/user", ()=> {
-        beforeEach((done) => {
-            database = [];
-            done();
-        });
 
-        it(" TC-201-1 When a required input is missing, a valid error should be returned", (done) => {
+    describe("UC-201 add users /api/user", ()=> {
+        //clears and fills the test database before each test starts in this describe block, also resets auto increment
+        beforeEach((done) => {
+            dbconnection.getConnection(function(err, connection) {
+                if (err) throw err;
+                connection.query('DELETE FROM user;', (error, result, field) => {
+                    connection.query('ALTER TABLE user AUTO_INCREMENT = 1;', (error, result, field) => {
+                        connection.query('INSERT INTO user (id, firstName, lastName, street, city, isActive, emailAdress, password, phoneNumber) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);', [1, 'Mariëtte', 'van den Dullemen', 'Groenstraat 10', 'Rotterdam' , 1, 'm.vandullemen@server.nl', 'pfefejW41!', '0687629321'], (error, result, field) => {
+                            connection.release();
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+        
+        it("TC-201-1 When a required input is missing when creating a user, a valid error should be returned", (done) => {
             chai.request(server).post("/api/user").send({
                 //firstName ontbreekt
                 lastName: "Rietveld",
                 street: "Van Wenastraat 31",
                 city: "Giessenburg",
-                password: "berOertE5!",
+                password: "wvqOertE5!",
                 emailAdress: "chevy@gmail.com"
             })
             .end((err,res) => {
@@ -30,5 +44,304 @@ describe("Manage Users /api/user",() => {
                 done();
             });
         });
+
+        it("TC-201-2 When the Emailadress format is invalid, a valid error should be returned", (done) => {
+            chai.request(server).post("/api/user").send({
+                firstName: "Chevy",
+                lastName: "Rietveld",
+                street: "Van Wenastraat 31",
+                city: "Giessenburg",
+                password: "wvqOertE5!",
+                emailAdress: "chevy.gmail.com"
+            })
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(400)
+                result.should.be.a("string").that.equals("The provided Emailadress format is invalid");
+                done();
+            });
+        });
+
+        it("TC-201-3 When the password format is invalid, a valid error should be returned", (done) => {
+            chai.request(server).post("/api/user").send({
+                firstName: "Chevy",
+                lastName: "Rietveld",
+                street: "Van Wenastraat 31",
+                city: "Giessenburg",
+                password: "secret",
+                emailAdress: "chevy@gmail.com"
+            })
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(400)
+                result.should.be.a("string").that.equals("Password must contain 8-15 characters which contains at least one lower- and uppercase letter, one special character and one digit");
+                done();
+            });
+        });
+
+        it("TC-201-4 When the user (email) already exists, a valid error should be returned", (done) => {
+            chai.request(server).post("/api/user").send({
+                firstName: "Chevy",
+                lastName: "Rietveld",
+                street: "Van Wenastraat 31",
+                city: "Giessenburg",
+                password: "wvqOertE5!",
+                emailAdress: "m.vandullemen@server.nl"
+            })
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(401)
+                result.should.be.a("string").that.equals("Could not add user, the email has already been taken");
+                done();
+            });
+        });
+        //token not yet taken into consideration
+        it("TC-201-5 User has been succesfully added", (done) => {
+            chai.request(server).post("/api/user").send({
+                firstName: "Chevy",
+                lastName: "Rietveld",
+                street: "Van Wenastraat 31",
+                city: "Giessenburg",
+                password: "wvqOertE5!",
+                emailAdress: "chevy@gmail.com"
+            })
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(201)
+                result.should.be.a("string").that.equals("User has been succesfully registered");
+                done();
+            });
+        });
     });
+
+    describe("UC-204 Details of a user /api/user", ()=> {
+        
+        //cant do this one yet as token isnt implemented yet, so its told to be skipped
+        xit("TC-204-1 Invalid token when retrieving user details", (done) => {
+            chai.request(server).get("/api/user/")
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(401)
+                result.should.be.a("string").that.equals("Search failed as user token is invalid");
+                done();
+            });
+        });
+
+        it("TC-204-2 User ID doesnt exist", (done) => {
+            chai.request(server).get("/api/user/356783")
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(401)
+                result.should.be.a("string").that.equals("User with ID 356783 could not be found");
+                done();
+            });
+        });
+
+        it("TC-204-3 User ID exists", (done) => {
+            chai.request(server).get("/api/user/1")
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(200)
+                assert.deepEqual(result, {
+                    id: 1,
+                    firstName: 'Mariëtte',
+                    lastName: 'van den Dullemen',
+                    isActive: 1,
+                    emailAdress: 'm.vandullemen@server.nl',
+                    password: 'pfefejW41!',
+                    phoneNumber: '0687629321',
+                    roles: 'editor,guest',
+                    street: 'Groenstraat 10',
+                    city: 'Rotterdam',
+                    })
+                done();
+            });
+        });
+    });
+
+    describe("UC-205 Updating a user /api/user", ()=> {
+
+        it("TC-205-1 When a required input is missing when updating a user, a valid error should be returned ", (done) => {
+            chai.request(server).put("/api/user/2").send({
+                //firstName is missing
+                lastName: "Rietveld",
+                street: "Van Wenastraat 31",
+                city: "Giessenburg",
+                password: "wvqOertE5!",
+                phoneNumber: "0651160300",
+                emailAdress: "chevy@gmail.com"
+            })
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(400)
+                result.should.be.a("string").that.equals("First name must be a string");
+                done();
+            });
+        });
+
+        it("TC-205-3 Invalid phonenumber when updating user", (done) => {
+            chai.request(server).put("/api/user/2").send({
+                firstName: "Chevy",
+                lastName: "Rietveld",
+                street: "Van Wenastraat 31",
+                city: "Giessenburg",
+                password: "wvqOertE5!",
+                phoneNumber: "4958",
+                emailAdress: "chevy@gmail.com"
+            })
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(400)
+                result.should.be.a("string").that.equals("Phonenumber must be 10 digits long, example: 0612345678");
+                done();
+            });
+        });
+
+        it("TC-205-4 User requested to be updated doesnt exist", (done) => {
+            chai.request(server).put("/api/user/999").send({
+                firstName: "Chevy",
+                lastName: "Rietveld",
+                street: "Van Wenastraat 31",
+                city: "Giessenburg",
+                password: "wvqOertE5!",
+                phoneNumber: "0651160300",
+                emailAdress: "chevy@gmail.com"
+            })
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(400)
+                result.should.be.a("string").that.equals("Update failed, user with ID 999 does not exist");
+                done();
+            });
+        });
+
+        //cant do this one yet as token/login function isnt implemented yet, so its told to be skipped
+        xit("TC-205-5 User isnt logged in when trying to update", (done) => {
+            chai.request(server).put("/api/user/").send({
+                //
+            })
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(401)
+                result.should.be.a("string").that.equals("Cant update user, as you are not logged in");
+                done();
+            });
+        });
+
+        it("TC-205-6 User succesfully updated", (done) => {
+            chai.request(server).put("/api/user/2").send({
+                firstName: "Chevy",
+                lastName: "Rietveld",
+                street: "Van Wenastraat 31",
+                city: "Giessenburg",
+                password: "wvqOertE5!",
+                phoneNumber: "0651160300",
+                emailAdress: "chevy@gmail.com"
+            })
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(200)
+                assert.deepEqual(result, {
+                    id: 2,                          
+                    firstName: 'Chevy',             
+                    lastName: 'Rietveld',           
+                    isActive: 1,                    
+                    emailAdress: 'chevy@gmail.com', 
+                    password: 'wvqOertE5!',         
+                    phoneNumber: '0651160300',      
+                    roles: 'editor,guest',          
+                    street: 'Van Wenastraat 31',    
+                    city: 'Giessenburg'                           
+                    })
+                done();
+            });
+        });
+    });
+
+    describe("UC-206 Deleting a user /api/user", ()=> {
+
+        it("TC-206-1 User requested to be deleted doesnt exist", (done) => {
+            chai.request(server).delete("/api/user/999")
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(404)
+                result.should.be.a("string").that.equals("User with ID 999 not found, and could not be deleted");
+                done();
+            });
+        });
+
+        //cant do this one yet as token/login function isnt implemented yet, so its told to be skipped
+        xit("TC-206-2 User isnt logged in when trying to delete", (done) => {
+            chai.request(server).delete("/api/user/2")
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(401)
+                result.should.be.a("string").that.equals("Cant delete user, as you are not logged in");
+                done();
+            });
+        });
+
+        //cant do this one yet as token/login function isnt implemented yet, so its told to be skipped
+        xit("TC-206-3 User isnt the owner of the user they are trying to delete", (done) => {
+            chai.request(server).delete("/api/user/2")
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(401)
+                result.should.be.a("string").that.equals("Cant delete user, as you are not the owner of this user");
+                done();
+            });
+        });
+
+        it("TC-206-4 User succesfully deleted", (done) => {
+            chai.request(server).delete("/api/user/2")
+            .end((err,res) => {
+                res.should.be.an("object")
+                let {status, result} = res.body;
+                status.should.equals(200)
+                result.should.be.a("string").that.equals("User with ID 2 succesfully deleted");
+                done();
+            });
+        });
+    });
+
+    // describe("UC-202 Overview of users /api/user", ()=> {
+
+    //     before((done) => {
+    //         dbconnection.getConnection(function(err, connection) {
+    //             if (err) throw err;
+    //             connection.query('DELETE FROM user;', (error, result, field) => {
+    //                 connection.release();
+    //                 done();
+    //             });
+    //         });
+    //     });
+
+    //     it("TC-202-1 Show zero users must return an empty list ", (done) => {
+    //         chai.request(server).get("/api/user").send({
+    //         })
+    //         .end((err,res) => {
+    //             res.should.be.an("object")
+    //             let {status, result} = res.body;
+    //             status.should.equals(401)
+    //             result.should.be.a("string").that.equals("No users were found");
+    //             done();
+    //         });
+    //     });
+        
+    // });
 });
